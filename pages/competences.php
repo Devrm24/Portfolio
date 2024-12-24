673878d49e35cf6e1b98e82b565b119c70d28a9c
Devrm24 <alexandre.simsek@sts-sio-caen.info>
2024-12-24 16:21:10 +0100

    ajout fichiers

---
+++ pages/competences.php
@@ -0,0 +1,43 @@
+<?php
+require 'vendor/autoload.php'; 
+
+use Symfony\Component\Yaml\Yaml;
+
+
+try {
+    $contenu = Yaml::parseFile('data/competences.yaml');
+} catch (Exception $e) {
+    echo 'Erreur lors du chargement du fichier YAML: ',  $e->getMessage();
+    exit;
+}
+
+$utilisateur = $contenu['utilisateur'] ?? [];
+$competences = $utilisateur['competences'] ?? [];
+?>
+
+<section id="page2" class="page page-competences">
+    <div class="container">
+        <div class="competences">
+            <h1>COMPÉTENCES</h1>
+            <?php
+               
+                foreach ($competences as $competence_domaine) {
+                   
+                    $domaine = htmlspecialchars($competence_domaine['domaine']);
+                    echo "<h2>$domaine</h2>";
+
+                   
+                    foreach ($competence_domaine['competences'] as $comp) {
+                        $compName = htmlspecialchars($comp['nom']);
+                        $niveau = (int)$comp['niveau']; 
+
+                        echo "<div class='skill'>";
+                        echo "<span class='skill-name'>$compName</span>";
+                        echo "<div class='progress-bar'><div class='progress' style='width: $niveau%;'></div></div>";
+                        echo "</div>";
+                    }
+                }
+            ?>
+        </div>
+    </div>
+</section>
